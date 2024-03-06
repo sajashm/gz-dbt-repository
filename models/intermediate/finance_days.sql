b63465a0d24754caee69930a41dabb3ac95118e3
saja <saja.66.71@gmail.com>
2024-03-06 14:46:07 +0000

    commit

--- models/intermediate/finance_days.sql
+++ models/intermediate/finance_days.sql
@@ -1,3 +1,4 @@
+{{ config(materialized='table') }}
 SELECT
    date_date,
    COUNT(orders_id) AS nb_transactions,
@@ -14,4 +15,6 @@ SELECT
 FROM {{ ref("int_orders_operational") }}
 GROUP BY date_date
 ORDER BY date_date DESC
+
+
 
